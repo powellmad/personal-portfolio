4a627746366dcbf9a66944f5bd9ce5bb2d8ad5e6
powellmad <madsnpowell@gmail.com>
2021-08-03 11:19:53 -0500

    trying /screen

--- src/html.js
+++ src/html.js
@@ -19,7 +19,7 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta property="og:image" content="https://madison-powell.netlify.app/" />
+        <meta property="og:image" content="https://madison-powell.netlify.app/screen" />
         {headComponents}
       </head>
       <body {...bodyAttributes}>
